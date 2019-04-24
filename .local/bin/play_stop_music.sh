3100812ea05e00792005e426295beacc2b6eea15
evandrocoan <evandrocoan@hotmail.com>
2019-04-24 13:16:41 -0300

    Set to play_stop_music.sh also send the Escape key because sometimes the player
    may be focused in a text typing box.

--- .local/bin/play_stop_music.sh
+++ .local/bin/play_stop_music.sh
@@ -9,6 +9,6 @@ target_window=$(xdotool search --limit 1 --all --pid $(pgrep "$window_title"))
 # xdotool key --window "$target_window" "$key_to_send"
 wmctrl -x -a "$window_title" &&
 sleep 0.5 &&
-xdotool key "$key_to_send" &&
+xdotool key Escape key "$key_to_send" &&
 xdotool windowactivate "$active_window" &&
 printf "%s Done!\\n" "$0"
