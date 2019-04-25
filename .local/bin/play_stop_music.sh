f608f64fa3e67a419e58fffd423951c66ee85af6
evandrocoan <evandrocoan@hotmail.com>
2019-04-25 15:46:05 -0300

    Allowed to play_stop_music.sh send custom commands to xdotool

--- .local/bin/play_stop_music.sh
+++ .local/bin/play_stop_music.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # To list active windows use `wmctrl -lx`
 window_title="$1"
-key_to_send="$2"
+key_to_send=$(printf '%q ' "${@:2}")
 
 active_window=$(xdotool getactivewindow)
 target_window=$(xdotool search --limit 1 --all --pid $(pgrep "$window_title"))
@@ -9,6 +9,6 @@ target_window=$(xdotool search --limit 1 --all --pid $(pgrep "$window_title"))
 # xdotool key --window "$target_window" "$key_to_send"
 wmctrl -x -a "$window_title" &&
 sleep 0.5 &&
-xdotool key Escape key "$key_to_send" &&
+xdotool $key_to_send &&
 xdotool windowactivate "$active_window" &&
-printf "%s Done!\\n" "$0"
+printf "Script '%s' sent '%s' to '%s'!\\n" "$0" "$key_to_send" "$window_title"
