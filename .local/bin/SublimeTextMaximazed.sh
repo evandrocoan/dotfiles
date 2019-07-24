fa4e18062396207fe24366c3c11e855ec106796b
evandrocoan <evandrocoan@hotmail.com>
2019-07-24 15:09:12 -0300

    Fixed SublimeTextMaximazed.sh not opening samba shares

--- .local/bin/SublimeTextMaximazed.sh
+++ .local/bin/SublimeTextMaximazed.sh
@@ -6,5 +6,9 @@ pushd `dirname $0` > /dev/null
 SCRIPT_FOLDER_PATH=`pwd`
 popd > /dev/null
 
+# https://stackoverflow.com/questions/3306007/replace-a-string-in-shell-script-using-a-variable
 command_line=$(printf '%q ' "${@:1}")
+command_line="$(printf "%s" "${command_line}" | sed -e "s/\\\\,/\,/g")"
+
+printf "command_line: %s\\n" "${command_line}"
 "${SCRIPT_FOLDER_PATH}/open_maximized.sh" "Sublime Text" /usr/bin/subl -n ${command_line}
