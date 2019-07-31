b902cf557f5d81b4bfcb1faee47546a5873cfda6
evandrocoan <evandrocoan@hotmail.com>
2019-07-31 15:56:34 -0300

    Fixed SublimeTextMaximazed.sh not working for files with parentesis and spaces

--- .local/bin/SublimeTextMaximazed.sh
+++ .local/bin/SublimeTextMaximazed.sh
@@ -2,13 +2,17 @@
 
 # Reliable way for a bash script to get the full path to itself?
 # http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
-pushd `dirname $0` > /dev/null
-SCRIPT_FOLDER_PATH=`pwd`
-popd > /dev/null
+pushd `dirname $0` > /dev/null;
+SCRIPT_FOLDER_PATH=`pwd`;
+popd > /dev/null;
 
 # https://stackoverflow.com/questions/3306007/replace-a-string-in-shell-script-using-a-variable
-command_line=$(printf '%q ' "${@:1}")
-command_line="$(printf "%s" "${command_line}" | sed -e "s/\\\\,/\,/g")"
+command_line=$(printf '%s ' "${@}" | sed -e 's/[[:space:]]*$//');
 
-printf "command_line: %s\\n" "${command_line}"
-"${SCRIPT_FOLDER_PATH}/open_maximized.sh" "Sublime Text" /usr/bin/subl -n ${command_line}
+# command_line=$(printf '%q ' "${@}" | sed -e 's/[[:space:]]*$//');
+# command_line="$(printf "%s" "${command_line}" | sed -E "s/\\\\(.)/\\1/g")";
+# "${SCRIPT_FOLDER_PATH}/open_maximized.sh" "Sublime Text" /usr/bin/subl -n "${@:1}";
+
+printf "sublime   command_line: '%s'\\n" "${command_line}";
+/usr/bin/subl -n "${command_line}";
+"${SCRIPT_FOLDER_PATH}/open_maximized.sh" "Sublime Text";
--- .local/bin/open_maximized.sh
+++ .local/bin/open_maximized.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 # run it with open_maximized.sh "Sublime Text" /usr/bin/subl -n
-command_line=$(printf '%q ' "${@:2}")
-eval "$command_line"
 
-# https://unix.stackexchange.com/questions/264684/maximize-window-without-window-manager
-while [ true ]
-do
-    FocusApp=`xdotool getwindowfocus getwindowname`
+function maximize() {
+    program_name="${1}";
+    shift;
 
-    if [[ "$FocusApp" == *"$1"* ]];
-    then
-        # xdotool key super+Up
-        wmctrl -ir $(xdotool getactivewindow) -b add,maximized_vert,maximized_horz
-        break
-    fi
+    # https://unix.stackexchange.com/questions/264684/maximize-window-without-window-manager
+    while [ true ]
+    do
+        FocusApp=`xdotool getwindowfocus getwindowname`;
 
-    sleep 0.5
-done
+        if [[ "$FocusApp" == *"${program_name}"* ]];
+        then
+            # xdotool key super+Up
+            wmctrl -ir $(xdotool getactivewindow) -b add,maximized_vert,maximized_horz;
+            break;
+        fi;
 
+        sleep 0.5;
+    done
+}
+
+program_name="${1}";
+shift;
+
+maximize "${program_name}" &
