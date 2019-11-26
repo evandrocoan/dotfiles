c482aa657a0952f07f8ac737493ed2d02ff9a8fa
evandrocoan <evandrocoan@hotmail.com>
2019-11-26 09:59:34 -0300

    Set to SublimeTextMaximazed.sh to open Sublime Text vanilla install when
    the default/main Sublime Text process is not opened.

--- .local/bin/SublimeTextMaximazed.sh
+++ .local/bin/SublimeTextMaximazed.sh
@@ -13,6 +13,21 @@ command_line=$(printf '%s ' "${@}" | sed -e 's/[[:space:]]*$//');
 # command_line="$(printf "%s" "${command_line}" | sed -E "s/\\\\(.)/\\1/g")";
 # "${SCRIPT_FOLDER_PATH}/open_maximized.sh" "Sublime Text" /usr/bin/subl -n "${@:1}";
 
-printf "sublime   command_line: '%s'\\n" "${command_line}";
-/usr/bin/subl -n "${command_line}";
+printf "sublime command_line: '%s'\\n" "${command_line}";
+
+# https://www.brianstorti.com/understanding-shell-script-idiom-redirect/
+if [[ -z "${log_file}" ]]; then
+	log_file=
+fi
+
+# https://askubuntu.com/questions/157779/how-to-determine-whether-a-process-is-running-or-not-and-make-use-it-to-make-a-c
+if pgrep -f "/opt/sublime_text/sublime_text" > /dev/null
+then
+    printf "Sublime Text is running...\\n";
+    /opt/sublime_text/sublime_text -n "${command_line}";
+else
+    printf "Sublime Text is NOT running...\\n";
+    "${SCRIPT_FOLDER_PATH}/run_as_user.sh" sublime /opt/sublime_text_vanilla/sublime_text -n "${command_line}";
+fi
+
 "${SCRIPT_FOLDER_PATH}/open_maximized.sh" "Sublime Text";
