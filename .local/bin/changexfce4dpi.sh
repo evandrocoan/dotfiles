229a2b28eb5aee72c9eac77ca224273e240d8d40
evandrocoan <evandrocoan@hotmail.com>
2021-06-27 20:19:46 -0300

    Set changexfce4dpi.sh value to 5 dpis for fine tune control

--- .local/bin/changexfce4dpi.sh
+++ .local/bin/changexfce4dpi.sh
@@ -5,9 +5,9 @@ oldvalue="$(xfconf-query -c xsettings -p /Xft/DPI)";
 
 if [[ "$1" == "up" ]];
 then
-    newvalue="$(( oldvalue + 10 ))"
+    newvalue="$(( oldvalue + 5 ))"
 else
-    newvalue="$(( oldvalue - 10 ))"
+    newvalue="$(( oldvalue - 5 ))"
 fi
 
 # https://superuser.com/questions/31917/is-there-a-way-to-show-notification-from-bash-script-in-ubuntu
