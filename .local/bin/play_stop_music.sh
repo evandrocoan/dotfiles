92fa8bef7802fd7fb39c01f3fd922bb17ce81c70
evandrocoan <evandrocoan@hotmail.com>
2021-06-27 20:19:17 -0300

    Added some usage references to play_stop_music.sh

--- .local/bin/play_stop_music.sh
+++ .local/bin/play_stop_music.sh
@@ -13,9 +13,11 @@ then
     ssh "$LOCAL_COMPUTER_SSH" 'schtasks //run //I //TN playpausemusicoverssh'
 fi
 
+# https://unix.stackexchange.com/questions/154546/how-to-get-window-id-from-xdotool-window-stack
 active_window=$(xdotool getactivewindow)
 target_window=$(xdotool search --limit 1 --all --pid $(pgrep "$window_title"))
 
+# https://superuser.com/questions/183680/gnome-ubuntu-how-to-bring-a-program-window-to-the-front-using-a-command-line
 # xdotool key --window "$target_window" "$key_to_send"
 wmctrl -x -a "$window_title" &&
 sleep 0.5 &&
