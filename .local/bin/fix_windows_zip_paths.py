1ba13b8319ee08f92f6513f72ceff77c1803b3a3
evandrocoan <evandrocoan@hotmail.com>
2023-05-22 14:03:59 -0300

    Set to fix_windows_zip_paths.py remove \uf05c (invalid utf8 char)

--- .local/bin/fix_windows_zip_paths.py
+++ .local/bin/fix_windows_zip_paths.py
@@ -6,12 +6,25 @@ import os
 made_dirs = set()
 
 for root, dir_names, file_names in os.walk('.'):
-    for file_name in file_names:
-        if '\\' not in file_name:
+    for file_name in dir_names + file_names:
+        # print( 'file_name', repr(file_name) )
+        continue_now = 0
+        bad_chars = ('\uf05c', '\\')
+        for bad in bad_chars:
+            if bad not in file_name:
+                continue_now += 1
+                break
+        if continue_now >= len(bad_chars):
             continue
-        alt_file_name = file_name.replace('\\', '/')
+
+        alt_file_name = file_name
+        for bad in bad_chars:
+            alt_file_name = alt_file_name.replace(bad, '/')
         if alt_file_name.startswith('/'):
             alt_file_name = alt_file_name[1:]  # cut of starting dir separator
+
+        if alt_file_name.find('/') < 0:
+            continue
         alt_dir_name, alt_base_name = alt_file_name.rsplit('/', 1)
         print( 'alt_dir', alt_dir_name )
         full_dir_name = os.path.join(root, alt_dir_name)
