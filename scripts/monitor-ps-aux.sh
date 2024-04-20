cb06077e5aa94dd6ed55c55e128357f407ce4ac6
evandrocoan <evandrocoan@hotmail.com>
2024-04-19 22:12:24 -0300

    Set to create the monitor-ps-aux.sh file accessible by the user

--- scripts/monitor-ps-aux.sh
+++ scripts/monitor-ps-aux.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
-OUTFILE=/root/monitor-ps-aux.log
-OUTFILE_OLD=/root/monitor-ps-aux-old.log
+set -TEeuo pipefail
+USERNAME="yourusername"
+
+USER=$(id -un $USERNAME)
+GROUP=$(id -gn $USERNAME)
+
+OUTFILE="/home/$USERNAME/monitor-ps-aux.log"
+OUTFILE_OLD="/home/$USERNAME/monitor-ps-aux-old.log"
 
 counter=0
+sleep_time=10
+rotation_time=$(( 60 * 60 * 48 / sleep_time ))
 
 while true
 do 
     counter=$((counter+1));
-	if [[ $((counter % 17280)) -eq 0 ]];
+	if [[ $((counter % rotation_time)) -eq 0 ]];
     then
         counter=0;
-        printf 'Clean up space on %s...\n' "${OUTFILE}";
+        printf '%s Clean up space on %s...\n' "$(date)" "${OUTFILE}";
+
         rm -v "${OUTFILE_OLD}";
         mv -v "${OUTFILE}" "${OUTFILE_OLD}";
+        chown $USER:$GROUP "${OUTFILE_OLD}";
     fi;
 
-	# printf "%s\n\n" "$(ps aux --sort=-pcpu)" >> "${OUTFILE}"
-	printf "%s\n\n" "$(ps aux --sort -rss)" >> "${OUTFILE}"
-	sleep 10
+	# ps aux --sort=-pcpu
+	printf "%s\n%s\n\n" "$(date)" "$(ps aux --sort -rss)" >> "${OUTFILE}";
+    chown $USER:$GROUP "${OUTFILE}";
+	sleep $sleep_time;
 done
 
 # sudo vim /etc/systemd/system/monitor-ps-aux.service
@@ -26,9 +37,10 @@ done
 # sudo systemctl start monitor-ps-aux.service
 # sudo systemctl status monitor-ps-aux.service
 # [Unit]
-# Description=Executa script monitor-ps-aux.sh to save ps aux data for last days
+# Description=Run script monitor-ps-aux.sh to save ps aux data for last days
 # After=network.target
-# StartLimitIntervalSec=0
+# StartLimitBurst=10
+# StartLimitIntervalSec=10
 
 # [Service]
 # Type=simple
